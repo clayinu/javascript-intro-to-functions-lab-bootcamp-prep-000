6798f20d7b23922e3dc95d69acde7aa16809b29e
Hoko Le <hokole527@gmail.com>
2019-08-15 20:56:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if () {
-    string.toLowerCase() === string
+  if (string.toLowerCase() === string) {
   string = "I can't hear you!"  
   return string
   }
